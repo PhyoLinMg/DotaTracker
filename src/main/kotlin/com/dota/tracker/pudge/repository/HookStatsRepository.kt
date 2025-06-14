7b7ca601e754da62aa796d59dc29d9a5036b9631
PhyoLinMg <gangsterplm@gmail.com>
2025-06-14 15:40:30 +0700

    update some components, set the id to matchid and add some apis

--- src/main/kotlin/com/dota/tracker/pudge/repository/HookStatsRepository.kt
+++ src/main/kotlin/com/dota/tracker/pudge/repository/HookStatsRepository.kt
@@ -2,5 +2,9 @@ package com.dota.tracker.pudge.repository
 
 import com.dota.tracker.pudge.model.HookStats
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
-interface HookStatsRepository : JpaRepository<HookStats, Long>
+interface HookStatsRepository : JpaRepository<HookStats, Long> {
+    @Query("SELECT h FROM HookStats h ORDER BY (CASE WHEN h.hookCasts > 0 THEN (h.hookHits * 1.0 / h.hookCasts) ELSE 0 END) DESC LIMIT 10")
+    fun findTop10ByHighestAccuracy(): List<HookStats>
+}
